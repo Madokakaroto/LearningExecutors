b5102a89ba775e92efd8c93d19afcbaf7f270735
Madokakaroto <krbu@tencent.com>
2021-05-16 19:16:20 +0800

    first source files

---
+++ Executors.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <exception>
+#include <concepts>
+
+namespace std
+{
+    //template <typename R, typename E = bad_exception>
+    //concept receiver =
+    //        move_con
+}
---
+++ main.cpp
@@ -0,0 +1,6 @@
+#include "Executors.hpp"
+
+int main(void)
+{
+    return 0;
+}
