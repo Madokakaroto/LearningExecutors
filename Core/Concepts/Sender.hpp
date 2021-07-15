df62b2918534c1cc2ae4f3f971221c384fd96160
krbu <krbu@tencent.com>
2021-07-15 16:27:27 +0800

    for msvc

--- Core/Concepts/Sender.hpp
+++ Core/Concepts/Sender.hpp
@@ -3,9 +3,19 @@
 namespace std::execution
 {
     template <typename S>
+    struct test_atomic_c
+    {
+        constexpr operator bool ()const{ return true; }
+        static constexpr bool value() noexcept
+        { 
+            return static_cast<bool>(!is_base_of_v<invalid_sender_traits, sender_traits<remove_cvref_t<S>>>);
+        }
+    };
+
+    template <typename S>
     concept sender =
         move_constructible<remove_cvref_t<S>> &&
-        negation_v<is_invalid_sender_traits<remove_cvref_t<S>>>;
+        test_atomic_c<S>::value();
 
     template <typename S, typename R>
     concept sender_to =
