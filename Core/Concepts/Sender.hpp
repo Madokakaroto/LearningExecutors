697aff9d432705e646e3c1abf586b89e6023b0e9
Madokakaroto <krbu@tencent.com>
2021-07-15 17:13:06 +0800

    support for gcc11, TODO ... to resolve atomic constraint depends on itself

--- Core/Concepts/Sender.hpp
+++ Core/Concepts/Sender.hpp
@@ -3,19 +3,12 @@
 namespace std::execution
 {
     template <typename S>
-    struct test_atomic_c
-    {
-        constexpr operator bool ()const{ return true; }
-        static constexpr bool value() noexcept
-        { 
-            return static_cast<bool>(!is_base_of_v<invalid_sender_traits, sender_traits<remove_cvref_t<S>>>);
-        }
-    };
-
-    template <typename S>
     concept sender =
-        move_constructible<remove_cvref_t<S>> &&
-        test_atomic_c<S>::value();
+        move_constructible<remove_cvref_t<S>> /*&&
+        !requires
+        {
+            typename sender_traits<remove_cvref_t<S>>::__unspecialized;
+        }*/;
 
     template <typename S, typename R>
     concept sender_to =
